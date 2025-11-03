9b13f7c53c516127644b39314becc2c4e7ad6547
ben <bensiriv@gmail.com>
2025-11-02 17:16:42 -0800

    this was the real change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex justify-center h-50'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
