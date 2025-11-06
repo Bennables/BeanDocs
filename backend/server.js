82fe126bec73f2f46b36e5222e840797af3cae4e
ben <bensiriv@gmail.com>
2025-11-05 21:21:55 -0800

    basic express endpoints

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,22 @@ app.use(cors());
 app.use(express.json());
 
 
-
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+  res.send('GETTING FROM THE MAIN URL');
 });
+
+app.post('/', (req, res) => {
+  res.send("POSTING TO THE MAIN URL")
+})
+
+app.put('/', (req, res) => {
+  res.send("PUTTING TO THE MAIN URL")
+})
+
+app.delete('/', (req, res) => {
+  res.send("DELETING ON THE MAIN URL")
+})
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
