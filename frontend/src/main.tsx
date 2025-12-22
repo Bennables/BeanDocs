b2e4abed3227ee3141b6f00845a48926bfa4279f
ben <bensiriv@gmail.com>
2025-12-22 12:04:49 -0800

    added lexical rich text editor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Editor2 from './editing.tsx'
 // import Editor from './components/lexical.tsx'
 import VSCodeGUI from './VSCodeGUI.tsx';
-import RichText from './richText.tsx';
+import RichText from './components/RichText.tsx';
 
 createRoot(document.getElementById('root')!).render(
 
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      {/* <Route path="/" element={<Home/>}/> */}
       <Route path="/edit" element={<Editor2 id={1}/>}/>
       <Route path="/gui" element={<VSCodeGUI/>}/>
-
+      <Route path="/" element={<RichText/>}/>
 
     </Routes>
   </BrowserRouter>
